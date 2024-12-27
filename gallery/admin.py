dc1e4406de8c202fd20c5cf98ca4a2c95e3908f3
jason <panelforgetechnologies@gmail.com>
2024-12-28 02:46:18 +1300

    Update Register models in admin.py

--- gallery/admin.py
+++ gallery/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import UserProfile, Image, Tag, Category, Report, Comment, Like, Dislike, Favorite, Album
 
-# Register your models here.
+# Registering models in the admin interface
+admin.site.register(UserProfile)
+admin.site.register(Image)
+admin.site.register(Tag)
+admin.site.register(Category)
+admin.site.register(Report)
+admin.site.register(Comment)
+admin.site.register(Like)
+admin.site.register(Dislike)
+admin.site.register(Favorite)
+admin.site.register(Album)
