279a56485bd6801cbcb3853c531c6cd405e4aefc
jason <panelforgetechnologies@gmail.com>
2024-12-28 04:04:29 +1300

    Added pagination to gallery view

--- gallery/views.py
+++ gallery/views.py
@@ -23,5 +23,18 @@ def upload_image(request):
 def gallery_view(request):
     # Get filtered images based on the current user
     images = Image.objects.get_filtered_images(request.user)
+
+    # Pagination: Show 20 images per page
+    paginator = Paginator(images, 20)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
     
-    return render(request, 'gallery.html', {'images': images})
+    return render(request, 'gallery.html', {'page_obj': page_obj})
+
+def image_detail(request, image_id):
+    # Example: Show a single image
+    try:
+        image = Image.objects.get(id=image_id)
+    except Image.DoesNotExist:
+        raise Http404("Image not found")
+    return render(request, 'image_detail.html', {'image': image})
