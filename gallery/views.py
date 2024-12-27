e41099077544a17088c60811279496fb5fd5d3a1
jason <panelforgetechnologies@gmail.com>
2024-12-28 03:06:36 +1300

    Added upload_image view

--- gallery/views.py
+++ gallery/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+# views.py
+from django.shortcuts import render, redirect
+from .forms import ImageUploadForm
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+@login_required
+def upload_image(request):
+    if request.method == 'POST':
+        form = ImageUploadForm(request.POST, request.FILES)
+        if form.is_valid():
+            # Save the image with the current user as the owner
+            image = form.save(commit=False)
+            image.user = request.user
+            image.save()
+            return redirect('gallery')  # Redirect to the gallery page after upload
+    else:
+        form = ImageUploadForm()
+
+    return render(request, 'upload_image.html', {'form': form})
